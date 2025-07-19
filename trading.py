19faf4be825d53e737c7597a62329b27dc9d08cc
6ayoob <tayoob632@gmail.com>
2025-07-19 18:02:15 +0300

    Update trading.py

--- trading.py
+++ trading.py
@@ -15,7 +15,7 @@ load_dotenv()
 API_KEY = os.getenv("tLgcha0kFzPnjIKGhQ")
 API_SECRET = os.getenv("YMeUOTHgyP59msCjxDfR0qAdHiCKJTo6ePSn")
 USE_TESTNET = os.getenv("USE_TESTNET", "False").lower() == "true"
-BOT_TOKEN = os.getenv("7800699278:AAEdMakvUEwysq-s0k9MsK6k4b4ucyHRfT4")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = int(os.getenv("CHAT_ID"))
 RISK_PERCENT = float(os.getenv("RISK_PERCENT", 10))
 POSITION_TRACKER_FILE = os.getenv("POSITION_TRACKER_FILE", "positions.json")
